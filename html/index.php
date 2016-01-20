f0cbf83261a42e8ef952584d3226de3f52345efd
User <user@MacBook-Pro-de-Julio-Zabadua.local>
2016-01-20 15:05:08 -0600

    Basic Languajes and change on noticias as object

--- html/index.php
+++ html/index.php
@@ -1,21 +1,26 @@
 <?php 
 
-	require __DIR__.'/vendor/autoload.php';
-	require __DIR__.'/core/hello.php';
+	require_once( __DIR__.'/vendor/autoload.php' );
+	require_once( __DIR__.'/core/controller.php' );
+	require_once( __DIR__.'/core/noticias.php' );
 
 	use PHPRouter\RouteCollection;
 	use PHPRouter\Config;
 	use PHPRouter\Router;
 	use PHPRouter\Route;
 
-	
-
 	$lang = $_GET["lang"];
 
 	$_SERVER["REQUEST_URI"] = str_replace( "/".$lang ,"",$_SERVER["REQUEST_URI"] );
-	
 
 	$collection = new RouteCollection();
+	
+
+	$collection->attachRoute(new Route('/news', array(
+	    '_controller' => 'Noticias::mostrarTodas',
+	    'parameters' => array("lang" => $lang) ,
+	    'methods' => 'GET'
+	)));
 
 	$collection->attachRoute(new Route('/gallery', array(
 	    '_controller' => 'Languages::showGallery',
@@ -23,11 +28,9 @@
 	    'methods' => 'GET'
 	)));
 
-	
+
 	
 	$router = new Router($collection);
 	$router->setBasePath('/');
 	$route = $router->matchCurrentRequest();
-
-
 ?>

634b947db5eeea52442a3c4e2b099e35125eba7b
Adan Serrano <adan@denumeris.com>
2016-01-20 13:30:23 -0600

    Se agrega un TEMPLATE de función BASE

--- html/core/base.php
+++ html/core/base.php
@@ -2,5 +2,29 @@
 
 require_once('conn.php');
 
+//var_dump($pdo);
 
-var_dump($pdo);
+/**
+ * Este es el template base para las funciones
+ * @param string $lang 
+ * @param array $args 
+ * @return type
+ */
+function funcion($lang,$args){
+
+	$default = array(
+		'valor' => 'valor',
+		'valordos' => $valordos,
+		'valortres' => 3
+		);
+	
+	$args = array_merge($default,$args);
+
+	if ($lang == "es"){
+
+		//Devuelve en español
+	}
+	else{
+		//Obviii en inglés
+	}
+}

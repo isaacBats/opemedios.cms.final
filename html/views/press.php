138cddadb5ff2e5db6076408d07b9f442bc13c86
Adan Serrano <adan@denumeris.com>
2016-01-22 09:59:46 -0600

    Se agrega la división entre BROCHURES Y PUBLICIDAD

--- html/views/press.php
+++ html/views/press.php
@@ -1,20 +1,36 @@
 <div id="content-press">
 <?php 
-
+$publicidad = "";
+$brochures = "";
 foreach ($galleries as $press) {
-	echo '
-	<article class="item4Col">
-	    <a href="/press/'.strtolower( $press["contexto"] ).'/'.$press["slug"].'">
-            <img style="width: 0%" 
-            alt="'.$press["nombre"].'" 
-            src="/images/press/cover/'.$press["imagen"].'">
-            <br class="clear">
-			<br class="clear">
-            '.$press["nombre"].'
-        </a>
-    </article>
-	';
+    if( $press['contexto'] == "publicity" ){
+        $publicidad .= '<article class="item4Col">
+                        <a href="/press/'.strtolower( $press["contexto"] ).'/'.$press["slug"].'">
+                            <img style="width: 0%" 
+                            alt="'.$press["nombre"].'" 
+                            src="/images/press/cover/'.$press["imagen"].'">
+                            <br class="clear">
+                            <br class="clear">
+                            '.$press["nombre"].'
+                        </a>
+                    </article>';    
+    }
+    else{
+        $brochures .= '<article class="item4Col">
+                        <a href="/press/'.strtolower( $press["contexto"] ).'/'.$press["slug"].'">
+                            <img style="width: 0%" 
+                            alt="'.$press["nombre"].'" 
+                            src="/images/press/cover/'.$press["imagen"].'">
+                            <br class="clear">
+                            <br class="clear">
+                            '.$press["nombre"].'
+                        </a>
+                    </article>';
+    }
+	
 }
+
+echo '<p class="tituloSeccion Publicidad">'.$this->trans($lang,'Publicidad', 'Publicity').'</p>'.$publicidad.'<br class="clear"><p class="tituloSeccion Publicidad">Brochures</p>'.$brochures;
 
 ?>
 </div><!-- #contenido -->

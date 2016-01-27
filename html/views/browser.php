d62283c13dbfa057b03c5121cf994070b813cd73
Julio Zebadua <jzebadua@denumeris.com>
2016-01-27 16:02:57 -0600

    Error de producto

--- html/views/browser.php
+++ html/views/browser.php
@@ -1,7 +1,7 @@
 <div class="search-box">
     	<form action="/catalog/browser" id="frmBuscar" novalidate="novalidate" method="POST" >
     	   	<input type="text" autocomplete="off" maxlength="50" id="q" name="q" placeholder="Búsqueda" class="text-label" onkeyup="getSearch( this.value );">
-    	   	<div id="autocomplete"></div>
+    	   	<!-- <div id="autocomplete"></div> -->
             <input type="submit" title="Buscar" value="Buscar" name="buscar">
 			<div class="listadoProductosAutoCompletar" id="listadoProductosAutoCompletar"></div>
            	<div style="clear:both;"></div>

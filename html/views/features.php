4534b43c20bd92e98e3fbc98a4cdf803b248e171
Julio Zebadua <jzebadua@denumeris.com>
2016-03-16 09:45:06 -0600

    Tops

--- html/views/features.php
+++ html/views/features.php
@@ -5,7 +5,17 @@
             </p>
             <p>
                 <?php  echo $this->trans($lang , 'Medidas cm' , 'Dimensions cm').': <b>W</b> '.$this->nmb($product['frente']).' <b>D</b> '.$this->nmb($product['fondo']).' <b>H</b> '.$this->nmb($product['altura']).' <b>cm</b><br>';?>
+
+                <?php echo  $this->trans($lang , 'Top with one extension' , 'Top with one extension').': 
+                <b>'.$this->nmb($product['extencion']).' cm</b><br><br>';?>
+
+                
+
+
                 <?php echo  $this->trans($lang , 'Medidas in' , 'Dimensions in').': <b>W</b> '.$product['frentre_plg'].' <b>D</b> '.$product['fondo_plg'].' <b>H</b> '.$product['altura_plg'].' <b>in</b><br>';?>
+
+                <?php echo  $this->trans($lang , 'Top with one extension' , 'Top with one extension').': 
+                <b>'.$this->nmb($product['extencion_plg']).' in</b><br><br>';?>
 
                 <?php if( $product["acabado"] != "-"){ echo $this->trans( $lang , "Acabado:" , "Finishes:"); ?><?php echo $product["acabado"]."<br>"; }?>
                 <?php if( $product["caracter"] != "-"){ echo $this->trans( $lang , "Carácter:" , "Character:");?> <?php echo $product['caracter']."<br>";} ?>

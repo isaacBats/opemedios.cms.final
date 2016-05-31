257c26f1e24bcb7c9e9c3dca825fe549e3f645e5
isaacBats <disa.indus@gmail.com>
2016-05-31 00:15:54 -0500

    Se cambian el query de noticias mandadas

--- html/Repositories/NoticiasRepository.php
+++ html/Repositories/NoticiasRepository.php
@@ -17,12 +17,19 @@ class NoticiasRepository extends BaseRepository{
 
 	public function showNewsToDay(){
 
-		$sql = ' 	SELECT n.id_noticia AS id, n.encabezado, f.nombre AS nameFont, a.id_empresa, e.nombre AS send
+		/*$sql = ' 	SELECT n.id_noticia AS id, n.encabezado, f.nombre AS nameFont, a.id_empresa, e.nombre AS send
 					FROM noticia n
 					INNER JOIN fuente f ON n.id_fuente = f.id_fuente
 					INNER JOIN asigna a ON n.id_noticia = a.id_noticia
 					INNER JOIN empresa e ON a.id_empresa = e.id_empresa
 					ORDER BY n.id_noticia DESC LIMIT 30;					
+				';*/
+
+		$sql = ' 	SELECT n.id_noticia AS id, n.encabezado, f.nombre AS nameFont
+					FROM noticia n
+					INNER JOIN fuente f ON n.id_fuente = f.id_fuente
+					WHERE fecha = CURDATE()
+					ORDER BY n.id_noticia DESC;					
 				';
 		$query = $this->pdo->prepare( $sql );
 

d79bc8b6cdb314fe1fcbb34b25d381688fcdb84c
isaacBats <disa.indus@gmail.com>
2016-11-14 00:13:30 -0600

    Actualizacion de la vista para agregar fuentes de periodicos con logo

--- html/controllers/Admin.FontPE.php
+++ html/controllers/Admin.FontPE.php
@@ -1,27 +1,29 @@
 <?php 
 
 include_once('Admin.Fonts.php');
-include_once(__DIR__.'/../Repositories/PeriodicoRepository.php');
-include_once(__DIR__.'/../Repositories/CoberturaRepository.php');
+use utilities\MediaDirectory;
 
 class AdminFontPE extends AdminFonts{
 
 	private $periodicoRepository;
 	private $coberturaRepository;
 	private $fuente;
+	private $urlLogo;
 
 	public function __construct(){
 
 		$this->periodicoRepository 	= new PeriodicoRepository();
 		$this->coberturaRepository 	= new CoberturaRepository();
 		$this->fuente 				= 'Periodico';
+		$this->urlLogo				= MediaDirectory::LOGO_FUENTES;
 	}
 
 	public function add(){
 		
 		if( isset( $_SESSION['admin'] ) ){
-			$campos = '';
-			require $this->adminviews . 'addFontPE.php';
+			ob_start();
+				require $this->adminviews . 'addFontPE.php';
+			$campos = ob_get_clean();
 			$this->addFont($campos, $this->fuente );
 		}else{
             header( "Location: http://{$_SERVER["HTTP_HOST"]}/panel/login");
@@ -33,11 +35,16 @@ class AdminFontPE extends AdminFonts{
 
 		if( !empty($_POST) ){
 
+			$logo = $_FILES['logo'];
 			$id_periodico = $this->periodicoRepository->idFuentePE();
 			$id_cobertura = $this->coberturaRepository->findIdByDescription($_POST['cobertura']);
 			$_POST['tipoFuente'] = $id_periodico;
 			$_POST['cobertura'] = $id_cobertura;
-			$_POST['logo'] = 'default.jpg';
+			
+			$adminColumn = new AdminColumns();
+			$guardarLogo = $adminColumn->saveImages( $logo, $this->urlLogo );
+
+			$_POST['logo'] = $guardarLogo['originName'];
 			if(isset($_POST['activo'])){
 				$_POST['activo'] = 1;
 			}else{
@@ -45,10 +52,18 @@ class AdminFontPE extends AdminFonts{
 			}
 			
 			if($this->periodicoRepository->addFontPE($_POST)){
-				header('Location: /panel/fonts/show-list');
 				// echo 'Se ha agregado una fuente de TV correctamente';
+				$alert = new stdClass();
+				$alert->tipo = 'alert-info';
+				$alert->mensaje = 'Se agrego la fuente <strong>' . $_POST['nombre'] . '</strong> Correctamente!!!';
+				$_SESSION['alerts']['fuentes'] = $alert;
+				header('Location: /panel/fonts/show-list');
 			}else{
-				echo 'No se agrego a la tabla fuente_per';
+				$alert = new stdClass();
+				$alert->tipo = 'alert-danger';
+				$alert->mensaje = 'No se agrego la fuente <strong>' . $_POST['nombre'] . '</strong> :(';
+				$_SESSION['alerts']['fuentes'] = $alert;
+				header( 'Location: ' . $_SERVER['HTTP_REFERER'] );
 			}
 			
 		}else{

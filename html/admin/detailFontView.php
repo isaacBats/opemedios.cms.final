5abc5dbaf3eb61849d7e84f57df940bd5a4c3102
isaacBats <disa.indus@gmail.com>
2016-11-16 00:46:32 -0600

    Fix en columna de data layer de informacion sobre la fuente

--- html/admin/detailFontView.php
+++ html/admin/detailFontView.php
@@ -2,10 +2,10 @@
 	<div class="col-sm-12">
 		<h1 class="page-header"><?= $font['nombre'] ?></h1>
 	</div>
-	<div class="col-md-4">
+	<div class="col-md-6">
 		<img src="/<?= $font['logo']  ?>" alt="<?= $font['nombre'] ?>" width="320">
 	</div>
-	<div class="col-md-8">
+	<div class="col-md-6">
 		<?php foreach ($font as $key => $value): 
 				if( $key != 'id_fuente' && $key != 'id_cobertura' && $key != 'logo' && $key != 'id_tipo_fuente' && $key != 'id_senal' ): ?>
 					<p><strong><?= ucwords( $key ) ?>: </strong><?= $value ?></p>

0795c32e0cbdc70117cceae7ee280af8f882ac13
isaacBats <disa.indus@gmail.com>
2017-01-08 00:32:42 -0600

    Avance de encabezado

--- html/models/Encabezado.php
+++ html/models/Encabezado.php
@@ -7,6 +7,7 @@ use utilities\Util;
 class Encabezado
 {
 	private $id;
+	private $id_noticia;
 	private $logo;
 	private $impactos;
 	private $costoXcm;
@@ -19,28 +20,34 @@ class Encabezado
 	private $tamanio;
 	private $tiraje;
 
-
-	// public function __construct( $logo, $pagina, $seccion, $tiraje, $fecha, $fraccion,  $id = 0 )
+	
 	public function __construct( array $encabezado )
 	{
 		$this->id = ( isset( $encabezado['id'] ) && !empty( $encabezado['id'] ) ) ? $encabezado['id'] : null;
+		$this->id_noticia = ( isset( $encabezado['id_noticia'] ) && !empty( $encabezado['id_noticia'] ) ) ? $encabezado['id_noticia'] : null;
 		$this->logo = ( isset( $encabezado['logo'] ) && !empty( $encabezado['logo'] ) ) ? $encabezado['logo'] : null;
 		$this->fecha = ( isset( $encabezado['fecha'] ) && !empty( $encabezado['fecha'] ) ) ? $encabezado['fecha'] : Util::getUnixDate();
 		$this->numeroPagina = ( isset( $encabezado['num_pagina'] ) && !empty( $encabezado['num_pagina'] ) ) ? $encabezado['num_pagina'] : 0;
 		$this->seccion = ( isset( $encabezado['seccion'] ) && !empty( $encabezado['seccion'] ) ) ? $encabezado['seccion'] : null;
 		$this->tiraje = ( isset( $encabezado['tiraje'] ) && !empty( $encabezado['tiraje'] ) ) ? $encabezado['tiraje'] : 0;		
-		// TODO: @Encabezado LA fraccion debe de comprender dos valores; el string con el valor en fraccion y del valor de la fraccion es decir un decimal.
-		$this->fraccion = ( isset( $encabezado['fraccion'] ) && !empty( $encabezado['fraccion'] ) ) ? $this->validaFraccion( $encabezado['fraccion'] ) : 0.0;
+		$this->fraccion = ( isset( $encabezado['fraccion'] ) && !empty( $encabezado['fraccion'] ) ) ? $this->validaFraccion( $encabezado['fraccion'] ) : [ 'float' => 0, 'string' => '0'];
+		$this->impactos = ( isset( $encabezado['impactos'] ) && !empty( $encabezado['impactos'] ) ) ? $encabezado['impactos'] : $this->buildImpactos();
+		$this->porcentaje = ( isset( $encabezado['porcentaje'] ) && !empty( $encabezado['porcentaje'] ) ) ? $encabezado['porcentaje'] : $this->buildPorcentaje();
+		$this->costoXcm = ( isset( $encabezado['costo_cm'] ) && !empty( $encabezado['costo_cm'] ) ) ? $encabezado['costo_cm'] : 0;
+		$this->costoNota = ( isset( $encabezado['costo_nota'] ) && !empty( $encabezado['costo_nota'] ) ) ? $encabezado['costo_nota'] : 0;
+		$this->tamanio = $this->id_noticia = ( isset( $encabezado['tamanio'] ) && !empty( $encabezado['tamanio'] ) ) ? $encabezado['tamanio'] : 0; 
+
 	}
 
 	private function validaFraccion( $fraccion )
 	{
-		if( is_float( $fraccion ) )
+		if( is_array( $fraccion ) )
 			return $fraccion;
 
 		$explode = explode('/', $fraccion );
-		
-		return $explode[0] / $explode[1];
+		$float = $explode[0] / $explode[1];
+
+		return [ 'string' => $fraccion, 'float' => $float ];
 	}
 
 	public function getFraccion()
@@ -48,18 +55,17 @@ class Encabezado
 		return $this->fraccion;
 	}
 
-	public function getImpactos()
+	public function buildImpactos()
 	{
-		$this->impactos = $this->tiraje * 3;
-
-		return $this->impactos;
+		if( $this->tiraje > 0 )
+			return  $this->tiraje * 3;
+		else
+			return null;
 	}
 
-	public function getPorcentaje()
+	public function buildPorcentaje()
 	{
-		$this->porcentaje = round( $this->getFraccion() * 100 ) / 100;
-
-		return $this->porcentaje;
+		return round( $this->getFraccion()['float'] * 100 );
 	}
 
 	public function getEncabezado()
